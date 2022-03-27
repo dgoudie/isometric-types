4660f0eb383aead999fe672403012a93427f4f16
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-03-27 13:00:24 -0500

    added workout model

--- src/models/workout.ts
+++ src/models/workout.ts
@@ -2,5 +2,25 @@ export interface IWorkout {
     userId: string;
     startedAt: string;
     endedAt?: string;
-    exerciseIds: string[];
+    exercises: IWorkoutExercise[];
+}
+
+export interface IWorkoutExercise {
+    _id: string;
+    sets: IWorkoutExerciseSet[];
+}
+
+export interface IWorkoutExerciseSet {
+    /**
+     * populated for weighted and assisted exercises. Should be negative for assisted exercises.
+     */
+    resistanceInPounds?: number;
+    /**
+     * populated for weighted and assisted exercises.
+     */
+    repetitions?: number;
+    /**
+     * only populated for 'timed' exercises
+     */
+    complete?: boolean;
 }
